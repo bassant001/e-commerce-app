035e04b307e83ec52f7c0f531e61d044619db75f
bassant001 <bassanta555532@gmail.com>
2026-03-27 02:05:07 +0200

    feat: initial commit

---
+++ src/app/_components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer className='bg-black text-white'>
+        <h3>footer</h3>
+        <h3>footer</h3>
+        <h3>footer</h3>
+        <h3>footer</h3>
+    </footer>
+  )
+}
